1fcd3ddbc9010c9f7e2ffe81a5678884995f49b1
Tim Schumacher <tim@bandenkrieg.hacked.jp>
2015-11-10 19:46:57 +0100

    Add some docs

--- config.dist.php
+++ config.dist.php
@@ -1,23 +1,29 @@
 <?php
+// get a app from https://apps.twitter.com/ and auth with it
 $settings = array(
-'oauth_access_token' => "",
-'oauth_access_token_secret' => "",
-'consumer_key' => "",
-'consumer_secret' => ""
+    'oauth_access_token' => "",
+    'oauth_access_token_secret' => "",
+    'consumer_key' => "",
+    'consumer_secret' => ""
 );
 
+// the username to fetch the tweets from
 $username = '';
+
+// the number of tweets to fetch
 $limit = 100;
 
+// the twig template
 $template = <<<EOT
-    ---
-    title: {{ tweet.title|replace({"\n" : ""})|raw }}
+---
+title: {{ tweet.title|replace({"\n" : ""})|raw }}
 date: {{ tweet.date }}
 taxonomy:
-category: blog
+    category: blog
 ---
 {{ tweet.html|raw }}
 EOT;
 
+// the directory to save the tweets to
 $dir = "";
 
